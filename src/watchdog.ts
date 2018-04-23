b88eb1d0ab2d002cf08083a2e26e01288f2c0706
Huan LI <zixia@zixia.net>
2018-04-23 14:46:32 +0800

    deploy for branch

--- src/watchdog.ts
+++ src/watchdog.ts
@@ -3,7 +3,8 @@
 
 import { EventEmitter } from 'events'
 
-import { log }  from 'brolog'
+import { Brolog }  from 'brolog'
+export const log = new Brolog()
 
 import { version }  from '../package.json'
 export const VERSION = version
